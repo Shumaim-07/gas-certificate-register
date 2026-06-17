6b195ef8d4a92a95e50b9a03dbf166330d3d8af1
Shumaim-07 <shamaim612@gmail.com>
2026-06-17 12:20:07 +0100

    improve form

--- src/App.tsx
+++ src/App.tsx
@@ -62,8 +62,7 @@ function AppRoutes() {
       <Route path="/certificate/:id" element={<EngineerGuard><CertificatePage /></EngineerGuard>} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
-      <Route path="/admin-demo" element={<AdminDashboardPage />} />
-    </Routes>
+      </Routes>
   )
 }
 
